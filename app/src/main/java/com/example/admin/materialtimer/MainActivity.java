76832bc9f85308f5875421b512660fce83853613
kmsolomo <kmsolomon@ucdavis.edu>
2018-02-01 20:50:24 -0800

    button styling

--- app/src/main/java/com/example/admin/materialtimer/MainActivity.java
+++ app/src/main/java/com/example/admin/materialtimer/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.admin.materialtimer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button controlButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        controlButton = findViewById(R.id.controlButton);
+
+        controlButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
