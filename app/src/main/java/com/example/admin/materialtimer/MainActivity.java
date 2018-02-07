36397af74c5fbc5be2500f21216a658065a254a7
kmsolomo <kmsolomon@ucdavis.edu>
2018-02-06 21:14:26 -0800

    timer setup and textview adjustment

--- app/src/main/java/com/example/admin/materialtimer/MainActivity.java
+++ app/src/main/java/com/example/admin/materialtimer/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.admin.materialtimer;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -20,10 +20,46 @@ public class MainActivity extends AppCompatActivity {
         controlButton = findViewById(R.id.controlButton);
         timerView = findViewById(R.id.timerTextView);
 
+        //set timer based on settings
+
+        //placeholder
+        timerView.setText("30");
+
+
+        //
+        final CountDownTimer firstTimer = new CountDownTimer(30000, 1000) {
+            @Override
+            public void onTick(long l) {
+                long timeInSeconds = l / 1000;
+                String currentTime = String.valueOf(timeInSeconds);
+                timerView.setText(currentTime);
+            }
+
+            @Override
+            public void onFinish() {
+                timerView.setText("00");
+            }
+        };
+
+        final CountDownTimer secondTimer = new CountDownTimer(60000, 1000) {
+            @Override
+            public void onTick(long l) {
+                long timeInSeconds = l / 1000;
+                String currentTime = String.valueOf(timeInSeconds);
+                timerView.setText(currentTime);
+            }
+
+            @Override
+            public void onFinish() {
+                timerView.setText("00");
+            }
+        };
+
         controlButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                firstTimer.start();
+                secondTimer.start();
             }
         });
 
