bb8b24db23e4df6091fca4ed8774a21eb07014fe
kmsolomo <kmsolomon@ucdavis.edu>
2018-02-15 20:43:34 -0800

    setup settings activity

--- app/src/main/java/com/example/admin/materialtimer/SettingsActivity.java
+++ app/src/main/java/com/example/admin/materialtimer/SettingsActivity.java
@@ -1,18 +1,18 @@
 package com.example.admin.materialtimer;
 
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
 import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by admin on 2/14/18.
  */
 
-public class SettingsActivity extends AppCompatActivity{
+public class SettingsActivity extends PreferenceActivity{
 
     @Override
     protected void onCreate(Bundle onSaveInstanceState){
         super.onCreate(onSaveInstanceState);
-
 
     }
 }
