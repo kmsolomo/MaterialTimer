3d62a6c769daa385c75b0044b135bc48ada6d2c8
kmsolomo <kmsolomon@ucdavis.edu>
2018-02-27 19:38:57 -0800

    settings options

--- app/src/main/java/com/example/admin/materialtimer/SettingsActivity.java
+++ app/src/main/java/com/example/admin/materialtimer/SettingsActivity.java
@@ -1,10 +1,13 @@
 package com.example.admin.materialtimer;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.view.MenuItem;
+import android.view.Window;
 
 /**
  * Created by admin on 2/14/18.
@@ -14,11 +17,25 @@ public class SettingsActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle onSaveInstanceState){
+
         super.onCreate(onSaveInstanceState);
 
         FragmentManager fragManager = getFragmentManager();
         FragmentTransaction fragTransaction = fragManager.beginTransaction();
         fragTransaction.replace(android.R.id.content,new SettingsFragment()).commit();
+
+        ActionBar bar = getActionBar();
+        bar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public static class SettingsFragment extends PreferenceFragment{
