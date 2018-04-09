a434ed10c47dc731f3cb58f14d7e7733e57521fc
kmsolomo <kmsolomon@ucdavis.edu>
2018-04-09 10:12:27 -0700

    WIP: async CountDownTimer

--- app/src/main/java/com/example/admin/materialtimer/MainActivity.java
+++ app/src/main/java/com/example/admin/materialtimer/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.admin.materialtimer;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.CountDownTimer;
 import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.app.Activity;
@@ -21,15 +19,10 @@ public class MainActivity extends Activity{
     private ImageButton settingsButton;
     private TextView timerView;
 
-    private SharedPreferences sharedPref;
-    private CountDownTimer timer;
-    private String [] keyChain = {"pref_theme_value","pref_work_time","pref_break_time",
-            "pref_long_break_time","pref_loop_amount"};
-    private String defaultTheme = "Dark";
-    private String appTheme,timerVal;
-    private final int THEME_REQUEST_CODE = 1;
+    private TimerUtility timer;
     private TimerState timerStatus = TimerState.Stopped;
     private long milliSecondsLeft;
+    private final int THEME_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,24 +35,22 @@ public class MainActivity extends Activity{
         timerView = findViewById(R.id.timerTextView);
         settingsButton = findViewById(R.id.SettingsButton);
 
-        initDefaultVal();
-        //milliSecondsLeft = convertTime(timerVal);
-        milliSecondsLeft = 10000;
-        TimerUtility.updateTimer(milliSecondsLeft,timerView);
+        PreferenceManager.setDefaultValues(this,R.xml.preferences,false);
+
+        milliSecondsLeft = 60000;
+        timer = new TimerUtility(timerView,this);
+        timer.updateTimer(milliSecondsLeft);
 
         //Floating action button
         controlButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(timerStatus == TimerState.Running){
-
-                    //TODO: Pause Timer
-                    stopTimer();
+                    timer.pauseTimer();
                     timerStatus = TimerState.Paused;
                     controlButton.setImageResource(R.drawable.ic_play_arrow_44dp);
                 } else {
-                    //TODO: Start Timer
-                    startTimer(milliSecondsLeft);
+                    timer.startTimer();
                     timerStatus = TimerState.Running;
                     controlButton.setImageResource(R.drawable.ic_pause_44dp);
                 }
@@ -80,9 +71,8 @@ public class MainActivity extends Activity{
     public void onResume(){
         super.onResume();
 
-        milliSecondsLeft = sharedPref.getLong("TimeLeft",0);
+        //milliSecondsLeft = sharedPref.getLong("TimeLeft",0);
 
-        restartTimer();
         //TODO: remove notification, remove background timer, update clock
     }
 
@@ -91,7 +81,6 @@ public class MainActivity extends Activity{
         super.onPause();
 
         if(timerStatus == TimerState.Running){
-            timer.cancel();
 
             //TODO: start background service & notification
 
@@ -102,7 +91,7 @@ public class MainActivity extends Activity{
 
         //Save Data
         //Testing
-        sharedPref.edit().putLong("TimeLeft",milliSecondsLeft).apply();
+        //sharedPref.edit().putLong("TimeLeft",milliSecondsLeft).apply();
     }
 
     @Override
@@ -110,36 +99,5 @@ public class MainActivity extends Activity{
         if(requestCode == THEME_REQUEST_CODE){
             recreate();
         }
-    }
-
-    public void restartTimer(){
-
-    }
-
-    public void startTimer(long timeLeft){
-
-        timer = new CountDownTimer(timeLeft, 1000) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-                milliSecondsLeft = millisUntilFinished;
-                TimerUtility.updateTimer(milliSecondsLeft,timerView);
-            }
-
-            @Override
-            public void onFinish() {
-
-            }
-        }.start();
-    }
-
-    public void stopTimer(){
-        timer.cancel();
-    }
-
-    public void initDefaultVal(){
-        //Set Default Preferences
-        PreferenceManager.setDefaultValues(getApplicationContext(),R.xml.preferences,false);
-        sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
-        timerVal = sharedPref.getString(keyChain[1],"");
     }
 }
--- app/src/main/java/com/example/admin/materialtimer/TimerUtility.java
+++ app/src/main/java/com/example/admin/materialtimer/TimerUtility.java
@@ -1,6 +1,10 @@
 package com.example.admin.materialtimer;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Handler;
+import android.preference.PreferenceManager;
+import android.os.CountDownTimer;
 import android.widget.TextView;
 
 /**
@@ -9,16 +13,75 @@ import android.widget.TextView;
 
 public class TimerUtility {
 
-    public static long converTime(String value){
+    public enum Timer {
+        Work, Break, LongBreak
+    }
+
+    private Timer timer;
+    private TextView timerView;
+    private Handler timerHandler;
+    private long milliSecondsLeft;
+    private int sessionBeforeLongBreak,sessionCount;
+    private SharedPreferences sharedPref;
+    private Runnable workRun,breakRun,longBreakRun;
+    private CountDownTimer workTimer,breakTimer,longBreakTimer;
+
+    private final String [] keyChain = {"pref_theme_value","pref_work_time","pref_break_time",
+            "pref_long_break_time","pref_loop_amount"};
+
+
+
+    public TimerUtility(TextView view,Context activity){
+        timerView = view;
+        sharedPref = PreferenceManager.getDefaultSharedPreferences(activity);
+        sessionBeforeLongBreak = 2;
+        sessionCount = 0;
+        timer = Timer.Work;
+        timerHandler = new Handler();
+        refreshTimers();
+    }
+
+    public void startTimer(){
+        switch(timer){
+            case Work:
+                timerHandler.post(workRun);
+                break;
+            case Break:
+                timerHandler.post(breakRun);
+                break;
+            case LongBreak:
+                timerHandler.post(longBreakRun);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void pauseTimer(){
+        //TODO: save timer state and ms left
+        if(timer == Timer.Work){
+            workTimer.cancel();
+        } else if(timer == Timer.Break){
+            breakTimer.cancel();
+        } else {
+            longBreakTimer.cancel();
+        }
+    }
+
+    public void stopTimer(){
+        //TODO: stop current timer and restore fresh state
+    }
+
+    public long convertTime(String value){
         return Long.valueOf(value) * 60000;
     }
 
-    public static void updateTimer(long milliSecondsLeft, TextView view){
+    public void updateTimer(long milliSecondsLeft) {
         int minutes = (int) milliSecondsLeft / 60000;
         int seconds = (int) milliSecondsLeft % 60000 / 1000;
         String currentTime = "";
 
-        if(minutes < 10){
+        if (minutes < 10) {
             currentTime = "0" + minutes;
         } else {
             currentTime += minutes;
@@ -26,12 +89,82 @@ public class TimerUtility {
 
         currentTime += ":";
 
-        if(seconds < 10){
+        if (seconds < 10) {
             currentTime += "0" + seconds;
         } else {
             currentTime += seconds;
         }
 
-        view.setText(currentTime);
+        timerView.setText(currentTime);
+    }
+
+    public void refreshTimers(){
+
+        //TODO: Get true values from shared preferences
+        final long workTime = 60000;
+        final long breakTime = 30000;
+        final long longBreakTime = 45000;
+
+        //Initialize CountDownTimer Runnable
+        //countDownInterval interval is < 1000 to counteract issue that skips onTick()
+        workRun = new Runnable(){
+            @Override
+            public void run(){
+                timer = Timer.Work;
+                workTimer = new CountDownTimer(workTime,100) {
+                    @Override
+                    public void onTick(long l) {
+                        updateTimer(l);
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        if(sessionCount < sessionBeforeLongBreak){
+                            timerHandler.post(breakRun);
+                        } else {
+                            timerHandler.post(longBreakRun);
+                        }
+                    }
+                }.start();
+            }
+        };
+
+        breakRun = new Runnable() {
+            @Override
+            public void run() {
+                timer = Timer.Break;
+                breakTimer = new CountDownTimer(breakTime,100) {
+                    @Override
+                    public void onTick(long l) {
+                        updateTimer(l);
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        sessionCount++;
+                        timerHandler.post(workRun);
+                    }
+                }.start();
+            }
+        };
+
+        longBreakRun = new Runnable(){
+            @Override
+            public void run() {
+                longBreakTimer = new CountDownTimer(longBreakTime,100) {
+                    @Override
+                    public void onTick(long l) {
+                        updateTimer(l);
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        sessionCount = 0;
+                        timerHandler.post(workRun);
+                    }
+                };
+            }
+        };
+
     }
 }
