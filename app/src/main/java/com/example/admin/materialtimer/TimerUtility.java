e8b094e573918387a89d564f17a186a7179dc4b1
kmsolomo <kmsolomon@ucdavis.edu>
2018-04-12 22:04:39 -0700

    replace listpreference with seekbar and expand TimerUtility functionality

--- app/src/main/java/com/example/admin/materialtimer/TimerUtility.java
+++ app/src/main/java/com/example/admin/materialtimer/TimerUtility.java
@@ -14,7 +14,7 @@ import android.widget.TextView;
 public class TimerUtility {
 
     public enum Timer {
-        Work, Break, LongBreak
+        Work, Break, LongBreak, Remaining
     }
 
     private Timer timer;
@@ -22,58 +22,130 @@ public class TimerUtility {
     private Handler timerHandler;
     private long milliSecondsLeft;
     private int sessionBeforeLongBreak,sessionCount;
+    private boolean sessionStart,customFlag;
     private SharedPreferences sharedPref;
     private Runnable workRun,breakRun,longBreakRun;
-    private CountDownTimer workTimer,breakTimer,longBreakTimer;
+    private CountDownTimer workTimer,breakTimer,longBreakTimer,customTimer;
 
-    private final String [] keyChain = {"pref_theme_value","pref_work_time","pref_break_time",
-            "pref_long_break_time","pref_loop_amount"};
+    private final String THEME_VALUE = "pref_theme_value";
+    private final String WORK_TIME = "pref_work_time";
+    private final String BREAK_TIME = "pref_break_time";
+    private final String LONG_BREAK_TIME = "pref_long_break_time";
+    private final String LOOP_AMOUT_VALUE = "pref_loop_amount";
 
 
 
     public TimerUtility(TextView view,Context activity){
         timerView = view;
         sharedPref = PreferenceManager.getDefaultSharedPreferences(activity);
-        sessionBeforeLongBreak = 2;
         sessionCount = 0;
         timer = Timer.Work;
+        sessionStart = false;
+        customFlag = false;
         timerHandler = new Handler();
         refreshTimers();
     }
 
     public void startTimer(){
-        switch(timer){
-            case Work:
-                timerHandler.post(workRun);
-                break;
-            case Break:
-                timerHandler.post(breakRun);
-                break;
-            case LongBreak:
-                timerHandler.post(longBreakRun);
-                break;
-            default:
-                break;
+//        switch(timer){
+//            case Work:
+//                timerHandler.post(workRun);
+//                break;
+//            case Break:
+//                timerHandler.post(breakRun);
+//                break;
+//            case LongBreak:
+//                timerHandler.post(longBreakRun);
+//                break;
+//            default:
+//                startCustomTimer(getTime());
+//                break;
+//        }
+        if(timer == Timer.Work && sessionStart == false){
+            sessionStart = true;
+            timerHandler.post(workRun);
+        } else {
+            startCustomTimer(getTime());
         }
     }
 
     public void pauseTimer(){
-        //TODO: save timer state and ms left
-        if(timer == Timer.Work){
-            workTimer.cancel();
-        } else if(timer == Timer.Break){
-            breakTimer.cancel();
+
+        if(customFlag){
+            saveTime();
+            customTimer.cancel();
         } else {
-            longBreakTimer.cancel();
+            switch(timer){
+                case Work:
+                    saveTime();
+                    workTimer.cancel();
+                    break;
+                case Break:
+                    saveTime();
+                    breakTimer.cancel();
+                    break;
+                case LongBreak:
+                    saveTime();
+                    longBreakTimer.cancel();
+                    break;
+                default:
+                    break;
+            }
         }
     }
 
-    public void stopTimer(){
+    public void saveTime(){
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putLong("timeLeft",milliSecondsLeft);
+        editor.apply();
+    }
+
+    public long getTime(){
+        return sharedPref.getLong("timeLeft",0);
+    }
+
+    public void resetTimer(){
         //TODO: stop current timer and restore fresh state
     }
 
-    public long convertTime(String value){
+    public long convertTime(int value){
         return Long.valueOf(value) * 60000;
+    }
+
+    public void startCustomTimer(long timeLeft){
+        customFlag = true;
+         customTimer = new CountDownTimer(timeLeft,300) {
+             @Override
+             public void onTick(long millisUntilFinished) {
+                 milliSecondsLeft = millisUntilFinished;
+                 updateTimer(milliSecondsLeft);
+             }
+
+             @Override
+             public void onFinish() {
+                 customFlag = false;
+                 switch (timer){
+                     case Work:
+                         if(sessionCount < sessionBeforeLongBreak){
+                             sessionCount++;
+                             timerHandler.post(breakRun);
+                         } else {
+                             timerHandler.post(longBreakRun);
+                         }
+                         break;
+                     case Break:
+                         timerHandler.post(workRun);
+                         break;
+                     case LongBreak:
+                         sessionCount = 0;
+                         timerHandler.post(workRun);
+                         break;
+                     default:
+                         break;
+                 }
+
+             }
+         }.start();
     }
 
     public void updateTimer(long milliSecondsLeft) {
@@ -101,27 +173,31 @@ public class TimerUtility {
     public void refreshTimers(){
 
         //TODO: Get true values from shared preferences
-        final long workTime = 60000;
-        final long breakTime = 30000;
-        final long longBreakTime = 45000;
+        final long workTime = 10000;
+        final long breakTime = 5000;
+        final long longBreakTime = 7000;
+        sessionBeforeLongBreak = sharedPref.getInt(LOOP_AMOUT_VALUE,4);
 
-        //Initialize CountDownTimer Runnable
-        //countDownInterval interval is < 1000 to counteract issue that skips onTick()
+        //Issue with CountDownTimer implementation
+        //CountDownInterval < 1000 as work around
         workRun = new Runnable(){
             @Override
             public void run(){
                 timer = Timer.Work;
-                workTimer = new CountDownTimer(workTime,100) {
+                workTimer = new CountDownTimer(workTime,300) {
                     @Override
                     public void onTick(long l) {
-                        updateTimer(l);
+                        milliSecondsLeft = l;
+                        updateTimer(milliSecondsLeft);
                     }
 
                     @Override
                     public void onFinish() {
                         if(sessionCount < sessionBeforeLongBreak){
+                            sessionCount++;
                             timerHandler.post(breakRun);
                         } else {
+                            sessionCount = 0;
                             timerHandler.post(longBreakRun);
                         }
                     }
@@ -133,15 +209,15 @@ public class TimerUtility {
             @Override
             public void run() {
                 timer = Timer.Break;
-                breakTimer = new CountDownTimer(breakTime,100) {
+                breakTimer = new CountDownTimer(breakTime,300) {
                     @Override
                     public void onTick(long l) {
-                        updateTimer(l);
+                        milliSecondsLeft = l;
+                        updateTimer(milliSecondsLeft);
                     }
 
                     @Override
                     public void onFinish() {
-                        sessionCount++;
                         timerHandler.post(workRun);
                     }
                 }.start();
@@ -151,18 +227,19 @@ public class TimerUtility {
         longBreakRun = new Runnable(){
             @Override
             public void run() {
-                longBreakTimer = new CountDownTimer(longBreakTime,100) {
+                timer = Timer.LongBreak;
+                longBreakTimer = new CountDownTimer(longBreakTime,300) {
                     @Override
                     public void onTick(long l) {
-                        updateTimer(l);
+                        milliSecondsLeft = l;
+                        updateTimer(milliSecondsLeft);
                     }
 
                     @Override
                     public void onFinish() {
-                        sessionCount = 0;
                         timerHandler.post(workRun);
                     }
-                };
+                }.start();
             }
         };
 
