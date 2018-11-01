9b2ad727a4aa34fd92582ad16e5148f68a0cce98
kmsolomo <kmsolomon@ucdavis.edu>
2018-10-31 21:32:02 -0700

    complete InjectorUtils test

--- app/src/test/java/com/kristoffersol/materialtimer/InjectorUtilsTest.java
+++ app/src/test/java/com/kristoffersol/materialtimer/InjectorUtilsTest.java
@@ -5,25 +5,20 @@ import com.kristoffersol.materialtimer.util.InjectorUtils;
 import com.kristoffersol.materialtimer.viewmodel.PomodoroViewModelFactory;
 
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
-import org.powermock.modules.junit4.rule.PowerMockRule;
 
 import static junit.framework.TestCase.assertEquals;
+
 
 @RunWith( PowerMockRunner.class )
 @PrepareForTest( InjectorUtils.class )
 public class InjectorUtilsTest {
-
-//    @Rule
-//    public PowerMockRule rule = new PowerMockRule();
 
     @Mock
     private PomodoroViewModelFactory factory;
@@ -33,29 +28,26 @@ public class InjectorUtilsTest {
 
     @Before
     public void setUp(){
-        MockitoAnnotations.initMocks(this);
-//        PowerMockito.mockStatic(InjectorUtils.class);
+        PowerMockito.mockStatic(InjectorUtils.class);
     }
 
     @Test
     public void shouldReturnViewModelFactory(){
-//        // set expectations
-//        Mockito.when(InjectorUtils.providePomodoroViewModelFactory()).thenReturn(factory);
-//
-//        // verify behavior
-//        PowerMockito.verifyStatic(InjectorUtils.class);
-//        InjectorUtils.providePomodoroViewModelFactory();
-        assertEquals(1,1);
+        //set expectations
+        Mockito.when(InjectorUtils.providePomodoroViewModelFactory()).thenReturn(factory);
+        assertEquals(factory, InjectorUtils.providePomodoroViewModelFactory());
+
+        //verify behavior
+        PowerMockito.verifyStatic(InjectorUtils.class);
+        InjectorUtils.providePomodoroViewModelFactory();
     }
 
     @Test
     public void shouldReturnPomodoroRepository(){
-//
-//        Mockito.when(InjectorUtils.providePomodoroRepository()).thenReturn(repository);
-//
-//        PowerMockito.verifyStatic(InjectorUtils.class);
-//        InjectorUtils.providePomodoroRepository();
+        Mockito.when(InjectorUtils.providePomodoroRepository()).thenReturn(repository);
+        assertEquals(repository, InjectorUtils.providePomodoroRepository());
 
-        assertEquals(1,1);
+        PowerMockito.verifyStatic(InjectorUtils.class);
+        InjectorUtils.providePomodoroRepository();
     }
 }
